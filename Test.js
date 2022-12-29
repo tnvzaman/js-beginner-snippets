7ff1ccb0507544ce33141dbba5dc3813598de028
Tanveer-Welldev <tanveer.zaman@welldev.io>
2022-12-29 12:08:30 +0600

    JS output practice

---
+++ Test.js
@@ -0,0 +1,13 @@
+var carName = "Volvo";
+var carName;
+
+let bikeName = "Volvo";
+// let bikeName = "Revolver";
+
+var x = 2;
+// let x = 3;
+
+console.log(carName + bikeName + x);
+
+carName = "BMW";
+console.log(carName);
